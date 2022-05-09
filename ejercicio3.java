7e06c21a78e993e4dfe04910a41843d0de375e4c
luiggi199917 <luiggi199917@gmail.com>
2022-05-09 11:41:49 -0500

    subiendo la pregunta 2

---
+++ ejercicio3.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args){
+        int number = 25500;
+
+        LinkedList<Integer> stack = new LinkedList<Integer>();
+        while(number > 0){
+            stack.push(number % 10);
+            number = number / 10;
+        }
+        while ((!stack.isEmpty())){
+            System.out.println(stack.pop());
+        }
+    }
+}  //Muchas gracias
